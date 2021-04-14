ae2b9b6c935a8c8bb462d00b7bfd30ff6e20c5f9
cherantmr <72267086+cherantmr@users.noreply.github.com>
2021-04-14 12:21:16 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,8 @@
+question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+check_button = "<br><br><button class='btn btn-info' onclick='check()'>check</button>";
+
+document.getElementById("output").innerHTML = row;
+
+document.getElementById("number1").value = "";
+document.getElementById("number2").value = "";
